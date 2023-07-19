8e109f71e576c9515e6e1eb9260496629ad849c0
DoooReyn <jl88744653@gmail.com>
2023-07-19 11:13:31 +0800

    添加一般通用接口

--- assets/scripts/supports/cmm/interface.ts
+++ assets/scripts/supports/cmm/interface.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: DoooReyn 
+ * @Date: 2023-07-18 20:32:18 
+ * @LastModifiedBy: DoooReyn 
+ * @LastModifiedAt: 2023-07-18 20:32:18 
+ */
+
 /**
  * 资源加载格式
  */
@@ -11,3 +18,35 @@ export interface AssetItem {
    */
   path: string;
 }
+
+/**
+ * 尺寸格式
+ */
+export interface SizeLike {
+  width: number;
+  height: number;
+}
+
+/**
+ * 坐标格式
+ */
+export interface PointLike {
+  x: number;
+  y: number;
+  z?: number;
+}
+
+/**
+ * 矩形格式
+ */
+export interface RectLike {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+/**
+ * 一般对象格式
+ */
+export type ObjectLike = Record<string | number | symbol, any>;
