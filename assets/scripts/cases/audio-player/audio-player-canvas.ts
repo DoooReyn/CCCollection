86fc7f5bb91d4cef7c709c4eaf71642ca252ff34
DoooReyn <jl88744653@gmail.com>
2023-07-20 21:50:49 +0800

    音频播放器预备

---
+++ assets/scripts/cases/audio-player/audio-player-canvas.ts
@@ -0,0 +1,39 @@
+/*
+ * @Author: DoooReyn
+ * @Date: 2023-07-18 22:22:40
+ * @LastModifiedBy: DoooReyn
+ * @LastModifiedAt: 2023-07-18 22:22:40
+ */
+
+import { _decorator, AudioClip, Component } from 'cc';
+import { Stores } from '../../supports/storage';
+import { Device } from '../../supports/device/device';
+import { Ciphers } from '../../supports/cipher/ciphers';
+import {
+  C_HOME_PAGE,
+  C_AES_KEY,
+  C_AES_IV,
+  C_SETTING,
+} from '../../supports/cmm/constants';
+import { ResLoader } from '../../supports/res/res-loader';
+const { ccclass } = _decorator;
+
+/**
+ * 音频播放器示例
+ */
+@ccclass('AudioPlayerCanvas')
+export class AudioPlayerCanvas extends Component {
+  start() {
+    Device.instance.init(C_HOME_PAGE);
+    Ciphers.AES.init(C_AES_KEY, C_AES_IV);
+    Stores.Advance.init(C_SETTING);
+    ResLoader.instance.loadDir({
+      dir: '/',
+      type: AudioClip,
+      onBad: (err) => console.error(err),
+      onOK: (clips) => {
+        console.log(clips);
+      },
+    });
+  }
+}
---
+++ assets/scripts/supports/audio-player/audio-player.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: DoooReyn
+ * @Date: 2023-07-18 20:36:38
+ * @LastModifiedBy: DoooReyn
+ * @LastModifiedAt: 2023-07-18 20:36:38
+ */
+
+import { _decorator, Component } from 'cc';
+const { ccclass } = _decorator;
+
+@ccclass('AudioPlayer')
+export class AudioPlayer extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
